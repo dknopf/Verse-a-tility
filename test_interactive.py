83073797e4bc84cb05850df203790eb20b221ab1
Daniel Knopf <danielkn609@gmail.com>
2019-10-26 16:51:36 -0400

    Removed examples and began building my own code for an executable

--- test_interactive.py
+++ test_interactive.py
@@ -1,16 +1,20 @@
 import tkinter as tk
-import spotify.py
+from spotify import create_top_ten
 
 root= tk.Tk()
 
-canvas1 = tk.Canvas(root, width = 300, height = 300)
-canvas1.pack()
 
-def hello ():
-    label1 = tk.Label(root, text= 'Hello World!', fg='green', font=('helvetica', 12, 'bold'))
-    canvas1.create_window(150, 200, window=label1)
+base_text = tk.Label(root, text="Please input your Spotify username")
+base_text.pack()
 
-button1 = tk.Button(text='Click Me',command=hello, bg='brown',fg='white')
-canvas1.create_window(150, 150, window=button1)
+# canvas1 = tk.Canvas(root, width = 300, height = 300)
+# canvas1.pack()
+#
+# def hello ():
+#     label1 = tk.Label(root, text= 'Hello World!', fg='green', font=('helvetica', 12, 'bold'))
+#     canvas1.create_window(150, 200, window=label1)
+#
+# button1 = tk.Button(text='Click Me',command=hello, bg='brown',fg='white')
+# canvas1.create_window(150, 150, window=button1)
 
 root.mainloop()
