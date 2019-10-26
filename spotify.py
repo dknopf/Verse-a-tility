63d079bd5b178b2c5456d6e7e2de3dfbc1dcce86
Nalu Tripician <nalutrip@icloud.com>
2019-10-26 16:46:17 -0400

    Created createTopTen function

--- spotify.py
+++ spotify.py
@@ -3,76 +3,77 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 from KNN import kNN
 
-client_id = "1cc2b52f7c6447409439ddc56223fb26"
-client_secret = "c1e05ecad59f4208aea0fb91d79fdbd4"
-uri = "https://dknopf.github.io/Verse-a-tility"
+def createTopTen(user):
+    client_id = "1cc2b52f7c6447409439ddc56223fb26"
+    client_secret = "c1e05ecad59f4208aea0fb91d79fdbd4"
+    uri = "https://dknopf.github.io/Verse-a-tility"
 
-username = "nalutrip"
-scope = "playlist-read-private,playlist-modify-private,user-read-private"
+    username = user
+    scope = "playlist-read-private,playlist-modify-private,user-read-private"
 
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #creates spotify object to access API
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #creates spotify object to access API
 
-token = util.prompt_for_user_token(username,scope,client_id=client_id,client_secret=client_secret,redirect_uri=uri)
+    token = util.prompt_for_user_token(username,scope,client_id=client_id,client_secret=client_secret,redirect_uri=uri)
 
-sp = spotipy.Spotify(auth=token)
+    sp = spotipy.Spotify(auth=token)
 
-userID = sp.me()['id']
-playlists = sp.user_playlists(username) #gives a Dictionary of user playlists
+    userID = sp.me()['id']
+    playlists = sp.user_playlists(username) #gives a Dictionary of user playlists
 
-songs = {}
+    songs = {}
 
-for playlist in playlists['items']:
+    for playlist in playlists['items']:
+        """
+        Each iteration of the loop gets all the songs for that playlist
+        """
+        if (playlist['owner']['id'] == username): #checks to see if it is a user created playlist vs a saved one
+            songDict = sp.user_playlist(username, playlist['id'], fields="tracks")
+            playlistSongs = songDict['tracks']
+
+            for i in range(len(playlistSongs['items'])):
+                try:
+                    songs[playlistSongs['items'][i]['track']['id']]=(playlistSongs['items'][i]['track']['name'],playlistSongs['items'][i]['track']['artists'][0]['name'])
+                except:
+                    print("empty boi") #for empty playlist
+
     """
-    Each iteration of the loop gets all the songs for that playlist
+    Dictionary (userSongs) Format:
+    songID: (songTitle,songArtist,(acousticness,danceability,energy,instrumentalness,liveness,loudness,speechiness,valence,tempo))
     """
-    if (playlist['owner']['id'] == username): #checks to see if it is a user created playlist vs a saved one
-        songDict = sp.user_playlist(username, playlist['id'], fields="tracks")
-        playlistSongs = songDict['tracks']
+    songList = songs.items()
+    userSongs = {}
 
-        for i in range(len(playlistSongs['items'])):
-            try:
-                songs[playlistSongs['items'][i]['track']['id']]=(playlistSongs['items'][i]['track']['name'],playlistSongs['items'][i]['track']['artists'][0]['name'])
-            except:
-                print("empty boi") #for empty playlist
+    for song in songList:
+        """
+        Audio analysis for all user songs
+        """
+        songID = song[0]
+        songTitle = song[1][0]
+        songArtist = song[1][1]
 
-"""
-Dictionary (userSongs) Format:
-songID: (songTitle,songArtist,(acousticness,danceability,energy,instrumentalness,liveness,loudness,speechiness,valence,tempo))
-"""
-songList = songs.items()
-userSongs = {}
+        features = sp.audio_features(songID)[0]
 
-for song in songList:
+        acousticness = features['acousticness']
+        danceability = features['danceability']
+        energy = features['energy']
+        instrumentalness = features['instrumentalness']
+        liveness = features['instrumentalness']
+        loudness = features['loudness']
+        speechiness = features['speechiness']
+        valence = features['valence']
+        tempo = features['tempo']
+
+        userSongs[songID] = (songTitle,songArtist,(acousticness,danceability,energy,instrumentalness,liveness,loudness,speechiness,valence,tempo))
+
     """
-    Audio analysis for all user songs
+    Finding top karaoke songs and Playlist Creation
+    <*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*>
     """
-    songID = song[0]
-    songTitle = song[1][0]
-    songArtist = song[1][1]
 
-    features = sp.audio_features(songID)[0]
+    top10 = kNN(10,userSongs)
 
-    acousticness = features['acousticness']
-    danceability = features['danceability']
-    energy = features['energy']
-    instrumentalness = features['instrumentalness']
-    liveness = features['instrumentalness']
-    loudness = features['loudness']
-    speechiness = features['speechiness']
-    valence = features['valence']
-    tempo = features['tempo']
+    playlistID = (sp.user_playlist_create(user = userID,name = "Your Top 10 Karaoke Songs! Found by Verse-a-tility.",public = False))['id']
 
-    userSongs[songID] = (songTitle,songArtist,(acousticness,danceability,energy,instrumentalness,liveness,loudness,speechiness,valence,tempo))
-
-"""
-Finding top karaoke songs and Playlist Creation
-<*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*>
-"""
-
-top10 = kNN(10,userSongs)
-
-playlistID = (sp.user_playlist_create(user = userID,name = "Your Top 10 Karaoke Songs! Found by Verse-a-tility.",public = False))['id']
-
-sp.user_playlist_add_tracks(user=userID,playlist_id=playlistID,tracks=top10,position=None)
+    sp.user_playlist_add_tracks(user=userID,playlist_id=playlistID,tracks=top10,position=None)
