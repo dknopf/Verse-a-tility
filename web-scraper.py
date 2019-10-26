9999fc2b7a8342d588f1782d19b3983491736011
Daniel Knopf <danielkn609@gmail.com>
2019-10-25 22:54:21 -0400

    Further added to find songs to scrape through the webpage using regexps

--- web-scraper.py
+++ web-scraper.py
@@ -26,10 +26,11 @@ def create_soup(url):
     soup = BeautifulSoup(content, "lxml") #creates a BeautifulSoup object, which represents the document as a nested data structed
     for link in soup.find_all("a"):
         # Create a temporary variable 'url' that is equal to the href attribute of link
-        url = link.get('href')
+        temp_url = link.get('href')
         # Checks whether the url is None or not and whether or not it is a valid url
-        if url != None and url[:5] == "https" and url[13:20] != "google":
-            find_songs(url)
+        if temp_url != None and temp_url[:5] == "https" and temp_url[13:20] != "google":
+            return find_songs(temp_url)
+
 
 
 # Takes a URL and finds all the strings of songs in it
@@ -37,6 +38,12 @@ def find_songs(url):
     # Create a temp webdriver
     content = set_up_webdriver_page_source(url)
     webpage_soup = BeautifulSoup(content, "lxml")
-    print(temporary_soup.title)
+    # page_text = webpage_soup.get_text()
+    between_quotes = re.compile('.*".*"')
+    match = webpage_soup.find(string=between_quotes)
+    # between_num_dash = re.compile('[0-9][0-9]*.* (- + -- + —)')
+    # result = between_quotes.search(page_text).group(0)
+    print(match)
+    return match
 
 create_soup("https://www.google.com/search?rlz=1C1CHFX_enUS704US704&ei=fYuzXdq9MY3b5gKxzITIBg&q=best+karaoke+songs&oq=best+karaoke+songs&gs_l=psy-ab.3..0i71l8.0.0..3240278...0.2..0.0.0.......0......gws-wiz.HjPC1IKlIYs&ved=0ahUKEwia8OGZzrjlAhWNrVkKHTEmAWkQ4dUDCAs&uact=5")
