d9730d3412366e4fb551a62fcc83f5e2a5e239a1
saolep <48069419+saolep@users.noreply.github.com>
2019-10-27 17:56:39 -0400

    Last thing

--- SetGenerator.py
+++ SetGenerator.py
@@ -40,32 +40,33 @@ def setGenerator(write,filepath):
             track = spotify.search(song, limit = 1, market = 'US')
             id = track['tracks']['items'][0]['id']
             name = track['tracks']['items'][0]['name']
-            ids.append((id,name))
+            popularity = track['tracks']['items'][0]['popularity']
+            ids.append((id,name,popularity))
         except:
             pass
 
     # Writes the CSV for all the song features for maybe implementing a better classifier
     if write:
         songcsv = open("song-features.csv",'w')
-        songcsv.write("Title, ID, Acousticness, Danceability, Energy, Instrumentalness, Liveness, Loudness, Speechiness, Valence, Tempo\n")
+        songcsv.write("Title, ID, Acousticness, Danceability, Energy, Instrumentalness, Liveness, Loudness, Speechiness, Valence, Tempo, Popularity\n")
 
     # Generating song features for each song given
     features = []
-    for id,name in ids:
+    for id,name,popularity in ids:
         fts = spotify.audio_features(id)[0]
         try:
-            flist = [name,id,fts['acousticness'],fts['danceability'],fts['energy'],fts['instrumentalness'],fts['liveness'],fts['loudness'],fts['speechiness'],fts['valence'],fts['tempo']]
+            flist = [name,id,fts['acousticness'],fts['danceability'],fts['energy'],fts['instrumentalness'],fts['liveness'],2**((fts['loudness'])/6),fts['speechiness'],fts['valence'],fts['tempo']/220,popularity/100]
             features.append(flist)
         except:
             pass
         if write:
-            songcsv.write(flist[0] + ',' + flist[1] + ',' + str(flist[2]) + ',' + str(flist[3]) + ',' + str(flist[4]) + ',' + str(flist[5]) + ',' + str(flist[6]) + ',' + str(flist[7]) + ',' + str(flist[8]) + ',' + str(flist[9]) + ',' + str(flist[10])+'\n')
+            songcsv.write(flist[0] + ',' + flist[1] + ',' + str(flist[2]) + ',' + str(flist[3]) + ',' + str(flist[4]) + ',' + str(flist[5]) + ',' + str(flist[6]) + ',' + str(flist[7]) + ',' + str(flist[8]) + ',' + str(flist[9]) + ',' + str(flist[10]) + ','+ str(flist[11]) + '\n')
     if write:
         songcsv.close()
 
     # Averaging all of the features together
     length = len(features)
-    avg = [0,0,0,0,0,0,0,0,0]
+    avg = [0,0,0,0,0,0,0,0,0,0]
     for list in features:
         for i in range(len(avg)):
             avg[i] += list[i+2]
@@ -75,13 +76,29 @@ def setGenerator(write,filepath):
     average = avg
     return average
 
+# res =setGenerator(True,"C:/Users/SEphron/Documents/GitHub/Verse-a-tility/song_list.csv")
+# results = ""
+# for i in res:
+#     results = results + (str(i) + ",")
 
+# open("avg.txt",'w').write(results)
 """
 Everything down here was just experimentation with the dictionary scaffolding of spotipy
 """
+# client_id = "1cc2b52f7c6447409439ddc56223fb26"
+# client_secret = "c1e05ecad59f4208aea0fb91d79fdbd4"
+# uri = "https://dknopf.github.io/Verse-a-tility"
+#
+#
+#
+# token = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret, proxies=None).get_access_token()
+# spotify = spotipy.Spotify(token)
+#
 # song = "Whip it"
 # track = spotify.search(song, limit = 1, market = 'US')
 # id = track['tracks']['items'][0]['id']
+# popularity = track['tracks']['items'][0]['popularity']
+# print(popularity)
 # fts = spotify.audio_features(id)[0]
 # comparison_tup = (fts['acousticness'],fts['danceability'],fts['energy'],fts['instrumentalness'],fts['liveness'],fts['loudness'],fts['speechiness'],fts['valence'],fts['tempo'])
 # print(comparison_tup)
