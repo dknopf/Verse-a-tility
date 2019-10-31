dcf194576d12f6a2a8f2e4522532255b08245f6c
Nalu Tripician <nalutrip@icloud.com>
2019-10-30 20:11:25 -0400

    started manalombis

--- manalonobis.py
+++ manalonobis.py
@@ -1,4 +1,11 @@
-def mahalonobis():
+import numpy as np
+
+def mahalonobis(x,data):
 """
 Calculates the Mahalonobis distance from the average values
 """
+
+    x_minus_mu = x- np.mean(data)
+    cov = np.cov(data)
+
+    
